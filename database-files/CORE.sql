57e77f3184ee6979045598b17d1a4c9eb3190928
Jeffrey Krapf <krapf.j@northeastern.edu>
2024-12-02 16:39:05 -0500

    added learning to database

--- database-files/CORE.sql
+++ database-files/CORE.sql
@@ -95,12 +95,13 @@ CREATE TABLE PositionTable (
 -- Step 5: Tables with Foreign Keys to Tables in Step 4
 CREATE TABLE PositionReview (
     PosReviewID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    Description VARCHAR(255),
+    Descriptions VARCHAR(255),
     Offer BOOL,
     ApplicationRating TINYINT(5),
     EnvironmentRating TINYINT(5),
     EducationRating TINYINT(5),
     ProfessionalRating TINYINT(5),
+    LearningRating, TINYINT(5)
     Applied BOOL,
     AppliedDate DATETIME,
     ResponseDate DATETIME,
@@ -156,6 +157,7 @@ CREATE TABLE PosStats (
     AppAmount INT,
     CallBackNum INT,
     Learning INT,
+    Environment INT,
     MeanResponseTime Time,
     CONSTRAINT PSPR FOREIGN KEY (PositionID)
         REFERENCES PositionTable(PositionID)
