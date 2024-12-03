ac87542926dff9d447dc3128428fdaea974277ad
samuelbaldwin05 <sambaldwin2005@gmail.com>
2024-12-02 22:54:27 -0500

    Changed page names

--- app/src/Home.py
+++ app/src/Home.py
@@ -57,13 +57,13 @@ if st.button("Act as John, a Political Strategy Advisor",
     logger.info("Logging in as Political Strategy Advisor Persona")
     st.switch_page('pages/00_Pol_Strat_Home.py')
 
-if st.button('Act as Mohammad, an USAID worker', 
+if st.button('Act as Samantha, a co-op reviewer',
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
+    st.session_state['first_name'] = 'Samantha'
+    st.switch_page('pages/10_Coop_Reviewer_Home.py')
 
 if st.button('Act as System Administrator', 
             type = 'primary', 
