b5b77da7c6f72b735c322bb6879374770c775e8a
camgiles <161501327+camgiles@users.noreply.github.com>
2024-12-03 13:17:09 -0500

    get method for /CompanyReview/{CompanyID}

--- api/backend/postings/postings_routes.py
+++ api/backend/postings/postings_routes.py
@@ -14,4 +14,23 @@ from backend.ml_models.model01 import predict
 # routes.
 postings = Blueprint('postings', __name__)
 
-#------------------------------------------------------------
+# Get all reviews for a company
+@postings.route('/CompanyReview/<CompanyID>', methods=['GET'])
+def get_company_reviews(CompanyID):
+    query = f'''
+        SELECT * FROM CompanyReview WHERE CompanyID = {CompanyID}
+    '''
+    
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for reviews
+    cursor.execute(query)
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # Create a HTTP Response object and add results of the query to it after "jsonify"-ing it.
+    response = make_response(jsonify(theData))
+    response.status_code = 200
+    return response
