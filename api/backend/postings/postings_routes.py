36112e8a8293a90e7741b41fcbbd01a882a7784f
camgiles <161501327+camgiles@users.noreply.github.com>
2024-12-03 14:34:25 -0500

    Post and delete for /CompanyReview/{CompanyID}

--- api/backend/postings/postings_routes.py
+++ api/backend/postings/postings_routes.py
@@ -34,3 +34,48 @@ def get_company_reviews(CompanyID):
     response = make_response(jsonify(theData))
     response.status_code = 200
     return response
+
+# Add a new review for a company
+@postings.route('/CompanyReview/<CompanyID>', methods=['POST'])
+def add_company_review(CompanyID):
+    # In a POST request, there is a collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    # extracting the variable
+    review_type = the_data['Type']
+    description = the_data['Description']
+    environment_rating = the_data['EnvironmentRating']
+    culture_rating = the_data['CultureRating']
+    
+    query = f'''
+        INSERT INTO CompanyReview (CompanyID, Type, Description, EnvironmentRating, CultureRating)
+        VALUES ({CompanyID}, '{review_type}', '{description}', {environment_rating}, {culture_rating})
+    '''
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    response = make_response("Successfully added review")
+    response.status_code = 200
+    return response
+
+# Delete a review by ComReviewID
+@postings.route('/CompanyReview/<ComReviewID>', methods=['DELETE'])
+def delete_company_review(ComReviewID):
+    query = f'''
+        DELETE FROM CompanyReview WHERE ComReviewID = {ComReviewID}
+    '''
+    current_app.logger.info(query)
+
+    # executing and committing the delete statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    response = make_response("Successfully deleted review")
+    response.status_code = 200
+    return response
