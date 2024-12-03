9b200bf77f4dec227cbff2ee22497ad8c4bfa087
justinzahler <zahler.j@northeastern.edu>
2024-12-03 12:26:03 -0500

    Route Headers
    Route Headers

---
+++ api/backend/reviews/reviews_routes.py
@@ -0,0 +1,10 @@
+########################################################
+# Routes for Reviews blueprint
+########################################################
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
---
+++ api/backend/stats/stats_routes.py
@@ -0,0 +1,10 @@
+########################################################
+# Routes for Stats blueprint
+########################################################
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
