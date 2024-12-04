5b191ce31db19ee5da76dd4a433578b06b43e747
Michael Maaseide <maaseidemichael@gmail.com>
2024-12-04 12:33:19 -0500

    ddl and other fixes

--- database-files/CORE.sql
+++ database-files/CORE.sql
@@ -95,12 +95,12 @@ CREATE TABLE JobPosting (
 -- Step 5: Tables with Foreign Keys to Tables in Step 4
 CREATE TABLE PositionReview (
     PosReviewID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    Description VARCHAR(255),
+    Description VARCHAR(500),
     Offer BOOL,
     ApplicationRating TINYINT(5),
     EnvironmentRating TINYINT(5),
     EducationRating TINYINT(5),
-    ProfessionalRating TINYINT(5),
+    EnjoymentRating TINYINT(5),
     Applied BOOL,
     AppliedDate DATETIME,
     ResponseDate DATETIME,
